c9d39d7aa93205806737bf7b6e5988f42c83c677
ealvarezf <ealvarez@grupou.mx>
2017-11-10 14:08:21 -0600

    Primer cambio usando Ramas

--- OPSystem/INV/MovAlmacen.aspx.vb
+++ OPSystem/INV/MovAlmacen.aspx.vb
@@ -1,6 +1,7 @@
 ﻿Imports Security_System
 'Ernesto Alvarez Flores
 'Prueba de colaboración
+'Prueba Colaboración 3
 Public Class MovAlmacen
     Inherits System.Web.UI.Page
     Private Ds As New DataSet
